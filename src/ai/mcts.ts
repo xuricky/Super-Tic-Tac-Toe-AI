aa089ace2da5e59ed3e2f8fd41e2a9b9747cb186
xuhang <1208900540@qq.com>
2019-03-26 01:32:01 +0800

    add createChild function

--- src/ai/mcts.ts
+++ src/ai/mcts.ts
@@ -17,16 +17,23 @@ namespace LimitCondition {
     export const depth = 5;
 }
 
+enum Evaluation {
+    global_all_success = 100,
+    global_associate_success = 40,
+    global_one_success = 10,
+    lcoal_associate_success = 4  
+}
+
 export class MCTS {
     static step: number = 1;
     private isAITurn: boolean;
     private hasChildren: boolean;
     private children: MCTS[];
     private parent: MCTS;
-    private move: number[][];
+    private move: number[];
     private evaluation: number;
     private position: Position;
-    constructor(parent: MCTS, isAITurn: boolean, move: number[][]) {
+    constructor(parent: MCTS, isAITurn: boolean, move: number[]) {
         this.parent = parent;
         this.isAITurn = isAITurn;
         this.move = move;
@@ -36,17 +43,28 @@ export class MCTS {
         this.position = this._getPosition();
     }
 
+    private _addChild(MCTSNode: MCTS) {
+        this.children.push(MCTSNode);
+    }
+
     private _getAction(): Action {
         return Action.getInstance();
     }
     
     private _evaluate(): number {
         let evaluation = 0;
+        let action = this._getAction();
+        if (this.isAITurn) {
+            let data = action.getActionData().OData;
+            
+        } else {
+            let data = action.getActionData().XData;
+        }
         return evaluation = 0;
     }
 
     private _getPosition(): Position {
-        let position = Position.lave;
+        let position = this.move[1] === 4 ? Position.center : [0, 2, 6, 8].some(n => n === this.move[1]) ? Position.corner : Position.lave;
         return position;
     }
 
@@ -58,8 +76,18 @@ export class MCTS {
     }
 
     private _createChild() {
+        const now = new Date().getTime();
         this.hasChildren = true;
         let strategy = this._getStrategy();
         let action = this._getAction();
+        let availablePos = action.getAvailablePos(this.move);
+        if (strategy === Strategy.random) {
+            
+        } else {
+            for (let i = 0; i < availablePos.length && new Date().getTime() - now < LimitCondition.cycle * 1000; i++) {
+                let MCTSNode = new MCTS(this, !this.isAITurn, this.move);
+                this._addChild(MCTSNode);
+            }
+        }
     }
 }
