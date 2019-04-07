f94d844871e1270d5d5114c11d2271a2de1f75df
xuricky <249269448@qq.com>
2019-04-07 16:41:27 +0800

    add banner

---
+++ src/components/banner.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+const bannerCss = require('../ui/css/banner.css');
+
+export class Banner extends React.Component {
+        render() {
+            return <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        }
+}
